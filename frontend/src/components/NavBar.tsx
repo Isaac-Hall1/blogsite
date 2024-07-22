f96ffe07e33ea63cdd12380c89951aab88da2bf3
Isaac Hall <106003051+Isaac-Hall1@users.noreply.github.com>
2024-07-21 19:11:19 -0500

    Update NavBar.tsx

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -6,9 +6,9 @@ function NavBar(){
             </div>
             <div className=" flex flex-grow items-center">
                 <div className="flex flex-initial text-sm flex-grow">
-                    <a href='http://localhost:5173/' className=" focus:text-teal-600  inline-block mt-0 text-teal-200 hover:text-white mr-4">Home</a>
-                    <a href='http://localhost:5173/createblog' className="  focus:text-teal-600 inline-block mt-0 text-teal-200 hover:text-white mr-4">Create Blog</a>
-                    <a href='http://localhost:5173/myposts' className="  focus:text-teal-600 inline-block mt-0 text-teal-200 hover:text-white mr-4">Your Posts</a>
+                    <a href='https://blogsite-nine-psi.vercel.app/' className=" focus:text-teal-600  inline-block mt-0 text-teal-200 hover:text-white mr-4">Home</a>
+                    <a href='https://blogsite-nine-psi.vercel.app/createblog' className="  focus:text-teal-600 inline-block mt-0 text-teal-200 hover:text-white mr-4">Create Blog</a>
+                    <a href='https://blogsite-nine-psi.vercel.app/myposts' className="  focus:text-teal-600 inline-block mt-0 text-teal-200 hover:text-white mr-4">Your Posts</a>
                 </div>
                 <div className="max-w-lg mx-auto">
                     <details className="open:ring-1 open:ring-slate-900 open:shadow-lg p-3 rounded-lg" >
@@ -18,13 +18,13 @@ function NavBar(){
                         <div className="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
                             <ul className="list-none lg:flex lg:items-center">
                                 <li className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 lg:mr-6">
-                                    <a href="http://localhost:5173/register">Register</a>
+                                    <a href="https://blogsite-nine-psi.vercel.app/register">Register</a>
                                 </li>
                                 <li className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 lg:mr-6">
-                                    <a href="http://localhost:5173/login">Login</a>
+                                    <a href="https://blogsite-nine-psi.vercel.app/login">Login</a>
                                 </li>
                                 <li className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-                                    <a href="http://localhost:5173/logout">Logout</a>
+                                    <a href="https://blogsite-nine-psi.vercel.app/logout">Logout</a>
                                 </li>
                             </ul>
                         </div>
