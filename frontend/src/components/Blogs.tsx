dedf01372500d3de5a0ba703e4e1658fa24fc8b5
Isaac Hall <106003051+Isaac-Hall1@users.noreply.github.com>
2024-07-21 21:31:23 -0500

    Update Blogs.tsx

--- frontend/src/components/Blogs.tsx
+++ frontend/src/components/Blogs.tsx
@@ -52,7 +52,7 @@ const BlogForm: React.FC<BlogFormProps> = () => {
           <div className="">
             <Editor
               apiKey='48wkofvujnrbtaqayadkh2m5uth2wl4df6an3x55gngq93yw'
-              onInit={(editor) => (editorRef.current = editor)}
+              onInit={(_event, editor) => (editorRef.current = editor)}
               init={{
                 plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate mentions tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
                 toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
